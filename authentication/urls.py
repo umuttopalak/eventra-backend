9d313c7f04566b0c9dd87e2f317d461732040b4e
Umut Topalak <umuttopalak@hotmail.com>
2024-08-31 00:59:05 +0300

    chore: Remove unnecessary import statement in authentication/urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,8 +3,6 @@ from django.urls import include, path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
-from .api import Home
-
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
