1a3f7e51c475c17f2503a34119b73d80a54cbbec
Umut Topalak <umuttopalak@hotmail.com>
2024-09-05 01:34:32 +0300

    feat: Add event app to INSTALLED_APPS and update API URLs
    This commit adds the 'event' app to the project's INSTALLED_APPS setting in the 'settings.py' file. It also updates the API URLs in the 'EVENTRA/urls.py' file to include the '/api/events' endpoint. This change allows the application to handle event-related API requests.

--- EVENTRA/settings.py
+++ EVENTRA/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'authentication',
     'location',
+    'event',
 ]
 
 MIDDLEWARE = [
--- EVENTRA/urls.py
+++ EVENTRA/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('api/admin', admin.site.urls),
     path('api/authentication/', include('authentication.urls')),
-    path('api/locations/', include('location.urls'))
+    path('api/locations/', include('location.urls')),
+    path('api/events', include('event.urls')),
 ]
