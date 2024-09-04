a6b3215fc1701d7f94df2fc3c2433feb022714c9
Umut Topalak <umuttopalak@hotmail.com>
2024-09-05 01:03:31 +0300

    chore: Add location app to INSTALLED_APPS and update API URLs

--- EVENTRA/urls.py
+++ EVENTRA/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),    
-    path('api/authentication', include('authentication.urls')),
+    path('api/admin', admin.site.urls),
+    path('api/authentication/', include('authentication.urls')),
+    path('api/locations/', include('location.urls'))
 ]
